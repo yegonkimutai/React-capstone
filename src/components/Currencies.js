5ec1353e7f225bd0d8347da7e21c7659a18c0be7
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-05-24 13:38:06 +0300

    :wrench:Update files

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const Currencies = () => {
     const navigation = useNavigate()
     const { currencies } = useSelector((store) => store.currencies)
-    const filtered = currencies.filter((currency) => currency.symbol)
+    const [search, setSearch] = useState('')
+
+    const filtered = currencies.filter((currency) => currency.symbol
+    .toLowerCase()
+    .includes(search.toLowerCase()))
+
+    
 
     const switchPage = (currency) => {
         navigation(`information/${currency.name}`, {state : {currency}})
@@ -12,14 +19,28 @@ const Currencies = () => {
 
     return(
         <div>
+            <h2>Currency Tracker</h2>
             <div>
-                <h2>Currency Tracker</h2>
+            <input
+                type="text"
+                placeholder="Search coin..."
+                className="search-input"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                role="button"
+                tabIndex={0}
+        />
             </div>
             <div>
                     {filtered.map((currency) => (
                         <div
                         key={currency.id}
                         onClick={() =>{switchPage(currency)}}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                              switchPage(currency);
+                            }
+                        }}
                         >
                             <h2>{currency.symbol}</h2>
                         </div>
